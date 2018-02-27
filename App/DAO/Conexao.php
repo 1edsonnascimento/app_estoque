d59f7c606ecf5be8ca1ef416d523de89a27e7b41
Edson do Nascimento <edson_nascimento1@hotmail.com>
2018-02-26 20:42:33 -0400

    alteraçoes do dia 26/02/2018

--- App/DAO/Conexao.php
+++ App/DAO/Conexao.php
@@ -11,5 +11,15 @@ namespace App\DAO;
 
 class Conexao
 {
+    protected $conexao;
+    private $database = "db_estoque";
+    private $host = "localhost";
+    private $user = "root";
+    private $senha = "Suporte99";
 
+    public function __construct()
+    {
+        $this->conexao = new \PDO("mysql:host={$this->host};dbname={$this->database}",$this->user,$this->senha);
+        $this->conexao->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION);
+    }
 }
